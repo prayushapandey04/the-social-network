0b27c681262d7d8f5d4724a13422cdf3db12c2ad
prayushapandey04 <prayusha.pandey4@gmail.com>
2023-12-10 19:47:38 -0600

    routes for thoughts including updating,creating,deleting,etc

---
+++ routes/api/routesThoughts.js
@@ -0,0 +1,19 @@
+const router = require('express').Router();
+
+const {
+    getThoughts,
+    getSingleThought,
+    createThought,
+    deleteThought, 
+    updateThought,
+    addReaction,
+    deleteReaction,
+} = require('../../controllers/thoughtController');
+
+router.route('/').get(getThoughts).post(createThought);
+
+router.route('/:thoughtId').get(getSingleThought).put(updateThought).delete(deleteThought);
+
+router.route('/:thoughtId/reactions').post(createReaction);
+
+router.route('/:thoughtId/reactions/:reactionId').delete(deleteReaction);
