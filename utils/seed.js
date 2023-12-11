cf8760a5a20d70023433d2293e8c402f688f2b38
prayushapandey04 <prayusha.pandey4@gmail.com>
2023-12-10 19:29:38 -0600

    seedds file

--- utils/seed.js
+++ utils/seed.js
@@ -1,3 +1,25 @@
 const connection = require('../config/connection');
 const { Thought, User } = require('../models');
 const { thoughtData, userData } = require('./data');
+
+connection.on('error', (err) => err);
+
+connection.once('open', async () => {
+    console.log('connection established!');
+
+    const checkThoughts = await connection.db.listCollections({ name: 'thoughts' }).toArray();
+    if (checkThoughts.length) {
+        await Thought.collection.drop();
+    }
+
+    const checkUser = await connection.db.listCollections({ name: 'users' }).toArray();
+    if (checkUser.length) {
+        await User.collection.drop();
+    }
+
+    await User.insertMany(userData);
+
+    console.table(userData);
+    console.info('Seeding is done~');
+    connection.close();
+})
